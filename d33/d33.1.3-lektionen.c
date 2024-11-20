fe6fdf9467da305dffa2098a95bdec3df7045883
tomas.kindahl@bahnhof.se <tomas.kindahl@bahnhof.se>
2024-11-20 09:03:09 +0100

    fel kod

--- d33/d33.1.3-lektionen.c
+++ d33/d33.1.3-lektionen.c
@@ -1,7 +1,14 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    pinMode(A5, INPUT);
+}
 
-int main(int argc, char **argv) {
-    return 0;
+void loop() {
+    // put your main code here, to run repeatedly:
+    int mic = analogRead(A5);
+    Serial.print(0); Serial.print(" ");
+    Serial.print(mic); Serial.print(" ");
+    Serial.println(1023);
+    delay(20);
 }
